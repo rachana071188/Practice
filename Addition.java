7080d3cc0cd3d19881ba8fbbe96b0851a238eeb8
rachana071188 <rachana.naveen7@gmail.com>
2023-12-16 11:23:17 +0530

    Addition Push

---
+++ Addition.java
@@ -0,0 +1,10 @@
+public class Addition {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
